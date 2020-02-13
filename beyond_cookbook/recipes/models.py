ca4232cc167edf9e903f9279933b0a7647d8f44d
halon <halon.filip@gmail.com>
2020-02-13 09:14:09 +0100

    RecipeManager added.

--- beyond_cookbook/recipes/models.py
+++ beyond_cookbook/recipes/models.py
@@ -2,6 +2,36 @@ from django.db import models
 from users.models import User
 from ingredients.models import Product
 
+
+class RecipeManager(models.Manager):
+    def add(self, *args, **kwargs):
+        recipe_attributes = {'name', 'description', 'owner', 'ingredients', 'servings'}
+        ingredient_attributes = {'name', 'description', 'image', 'api_id', 'api_unit', 'quantity'}
+        ingredients = {}
+        for attribute, value in kwargs:
+            assert attribute in recipe_attributes
+            if attribute == 'ingredients':
+                ingredients = value
+            else:
+                setattr(self, attribute, value)
+        self.save()
+        for item in ingredients:
+            ingredient = Product.objects.get(item["api_id"])
+            if not ingredient:
+                ingredient = Product.objects.create(
+                    name=item["name"],
+                    description=item["description"],
+                    image=item["image"],
+                    api_id=item["api_id"]
+                )
+            RecipeIngredients.objects.create(
+                recipe=self,
+                product=ingredient,
+                quantity=item["quantity"],
+                api_unit=item["api_unit"]
+            )
+
+
 class Recipe(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
