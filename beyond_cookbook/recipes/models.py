eb460a95831bd751bbd74481ded2a34ea6556504
Romana Ciesielska <r.ciesielska@gmail.com>
2020-02-13 21:43:05 +0100

    JWT

--- beyond_cookbook/recipes/models.py
+++ beyond_cookbook/recipes/models.py
@@ -2,6 +2,38 @@ from django.db import models
 from users.models import User
 from ingredients.models import Product
 
+
+class RecipeManager(models.Manager):
+    def add(self, *args, **kwargs):
+        recipe_attributes = {'name', 'description', 'owner', 'ingredients', 'servings'}
+        ingredient_attributes = {'name', 'description', 'image', 'api_id', 'api_unit', 'quantity'}
+        recipe = Recipe.objects.create(
+            name=kwargs["description"],
+            description=kwargs["description"],
+            # owner=User.objects.get(id=kwargs["owner"]),
+            servings=kwargs["servings"]
+        )
+        ingredients = kwargs["ingredients"]
+        for item in ingredients:
+            try:
+                ingredient = Product.objects.get(api_id=item["api_id"])
+            except Product.DoesNotExist:
+                ingredient = None
+            if not ingredient:
+                ingredient = Product.objects.create(
+                    name=item["name"],
+                    description=item["description"],
+                    # image=item["image"],
+                    api_id=item["api_id"]
+                )
+            RecipeIngredients.objects.create(
+                recipe=recipe,
+                product=ingredient,
+                quantity=item["quantity"],
+                api_unit=item["api_unit"]
+            )
+
+
 class Recipe(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
@@ -11,15 +43,18 @@ class Recipe(models.Model):
         through='RecipeIngredients'
     )
     servings = models.SmallIntegerField(default=1)
+    objects = models.Manager()
+    recipe = RecipeManager()
 
     def __str__(self):
         return self.name
+
 
 class RecipeIngredients(models.Model):
     recipe = models.ForeignKey(Recipe, null=True, on_delete=models.SET_NULL)
     product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     quantity = models.DecimalField(max_digits=32, decimal_places=16)
-    api_unit = models.IntegerField(default=1)
+    api_unit = models.CharField(max_length=255)
 
     class Meta:
         verbose_name = 'RecipeIngredients'
